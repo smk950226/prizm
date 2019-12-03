d776912c08451ec189d4d1a6ba6f82cf4aaf6902
smk950226 <smk950226@naver.com>
2019-12-03 23:01:38 +0900

    deploy

--- prizm_server/front/src/components/App/presenter.js
+++ prizm_server/front/src/components/App/presenter.js
@@ -49,7 +49,7 @@ import Modal from 'react-modal';
 Modal.setAppElement('#root')
 
 const App = (props) => {
-    if(!props.admin){
+    if(props.admin){
         return(
             <AdminRouteContainer initAdmin={props.initAdmin} profile={props.profile} isLoggedIn={props.isLoggedIn} showBtmNav={props.showBtmNav} photographer={props.photographer} chatList={props.chatList} />
         )
